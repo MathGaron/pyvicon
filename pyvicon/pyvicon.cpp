5c8ef11c41850a3fce9485b51278a0dfcd8c099d
MathGaron <mathieugaron91@gmail.com>
2017-12-08 12:32:52 -0500

    add connection wrap

--- pyvicon/pyvicon.cpp
+++ pyvicon/pyvicon.cpp
@@ -1,5 +1,6 @@
 #include <Python.h>
 #include <string>
+#include <iostream>
 #include "DataStreamClient.h"
 
 using namespace ViconDataStreamSDK;
@@ -9,14 +10,130 @@ using namespace CPP;
 extern "C" {
 #endif // __cplusplus
 
+    //################
+    //Module wrapped functions
+    //################
+
     void destroy_client(PyObject* client)
     {
-        delete (Client*)PyCapsule_GetPointer(client, "client");
+        delete (Client*)PyCapsule_GetPointer(client, "pyvicon.client");
     }
 
     static PyObject* new_client(PyObject* self, PyObject* args)
     {
-        return PyCapsule_New(new Client(), "client", destroy_client);
+        return PyCapsule_New(new Client(), "pyvicon.client", destroy_client);
+    }
+
+    static PyObject* pyvicon_version(PyObject* self, PyObject* args)
+    {
+        PyObject* capsule;
+        if (!PyArg_ParseTuple(args, "O", &capsule)) return NULL;
+
+        Client* client = (Client*)PyCapsule_GetPointer(capsule, "pyvicon.client");
+        Output_GetVersion out = client->GetVersion();
+        return Py_BuildValue("III", out.Major, out.Minor, out.Point);
+    }
+
+    //###################
+    //Connection handling
+    //###################
+
+    static PyObject* pyvicon_connect(PyObject* self, PyObject* args)
+    {
+        PyObject* capsule;
+        char* ip;
+        if (!PyArg_ParseTuple(args, "Os", &capsule, &ip)) return NULL;
+        Client* client = (Client*)PyCapsule_GetPointer(capsule, "pyvicon.client");
+
+        //Release GIL for io
+        Output_Connect out;
+        Py_BEGIN_ALLOW_THREADS
+        out = client->Connect(ip);
+        Py_END_ALLOW_THREADS
+        return Py_BuildValue("I", out.Result);
+    }
+
+    static PyObject* pyvicon_connect_to_multicast(PyObject* self, PyObject* args)
+    {
+        PyObject* capsule;
+        char* ip;
+        char* multicast;
+        if (!PyArg_ParseTuple(args, "Oss", &capsule, &ip, &multicast)) return NULL;
+        Client* client = (Client*)PyCapsule_GetPointer(capsule, "pyvicon.client");
+
+        //Release GIL for io
+        Output_ConnectToMulticast out;
+        Py_BEGIN_ALLOW_THREADS
+        out = client->ConnectToMulticast(ip, multicast);
+        Py_END_ALLOW_THREADS
+        return Py_BuildValue("I", out.Result);
+    }
+
+    static PyObject* pyvicon_disconnect(PyObject* self, PyObject* args)
+    {
+        PyObject* capsule;
+        if (!PyArg_ParseTuple(args, "O", &capsule)) return NULL;
+        Client* client = (Client*)PyCapsule_GetPointer(capsule, "pyvicon.client");
+
+        Output_Disconnect out;
+        out = client->Disconnect();
+        return Py_BuildValue("I", out.Result);
+    }
+
+    static PyObject* pyvicon_isconnected(PyObject* self, PyObject* args)
+    {
+        PyObject* capsule;
+        if (!PyArg_ParseTuple(args, "O", &capsule)) return NULL;
+
+        Client* client = (Client*)PyCapsule_GetPointer(capsule, "pyvicon.client");
+        if (client->IsConnected().Connected)
+            Py_RETURN_TRUE;
+        else
+            Py_RETURN_FALSE;
+    }
+
+    static PyObject* pyvicon_start_transmitting_multicast(PyObject* self, PyObject* args)
+    {
+        PyObject* capsule;
+        char* server_ip;
+        char* multicast_ip;
+        if (!PyArg_ParseTuple(args, "Oss", &capsule, &server_ip, &multicast_ip)) return NULL;
+        Client* client = (Client*)PyCapsule_GetPointer(capsule, "pyvicon.client");
+
+        //Release GIL for io
+        Output_StartTransmittingMulticast out;
+        Py_BEGIN_ALLOW_THREADS
+        out = client->StartTransmittingMulticast(server_ip, multicast_ip);
+        Py_END_ALLOW_THREADS
+        return Py_BuildValue("I", out.Result);
+    }
+
+    static PyObject* pyvicon_stop_transmitting_multicast(PyObject* self, PyObject* args)
+    {
+        PyObject* capsule;
+        if (!PyArg_ParseTuple(args, "O", &capsule)) return NULL;
+        Client* client = (Client*)PyCapsule_GetPointer(capsule, "pyvicon.client");
+
+        //Release GIL for io
+        Output_StopTransmittingMulticast out;
+        Py_BEGIN_ALLOW_THREADS
+        out = client->StopTransmittingMulticast();
+        Py_END_ALLOW_THREADS
+        return Py_BuildValue("I", out.Result);
+    }
+
+    //###################
+    // Configurations
+    //###################
+
+    static PyObject* pyvicon_set_buffer_size(PyObject* self, PyObject* args) {
+        PyObject* capsule;
+        unsigned int size;
+        if (!PyArg_ParseTuple(args, "OI", &capsule, &size)) return NULL;
+        Client* client = (Client*)PyCapsule_GetPointer(capsule, "pyvicon.client");
+
+        client->SetBufferSize(size);
+        Py_RETURN_NONE;
     }
 
     //###################
@@ -37,8 +154,19 @@ extern "C" {
         return NULL;
     }
 
+    //#####################
+    // Methods
+    //#####################
     static PyMethodDef PyviconMethods[] = {
          {"new_client", new_client, METH_VARARGS, "Create a Client instance"},
+         {"pyvicon_version", pyvicon_version, METH_VARARGS, "Get SDK function"},
+         {"pyvicon_connect", pyvicon_connect, METH_VARARGS, "Establish connection"},
+         {"pyvicon_connect_to_multicast", pyvicon_connect_to_multicast, METH_VARARGS, "Establish a multicast connection"},
+         {"pyvicon_disconnect", pyvicon_disconnect, METH_VARARGS, "Disconnect from connection"},
+         {"pyvicon_isconnected", pyvicon_isconnected, METH_VARARGS, "Verify if connection is established"},
+         {"pyvicon_start_transmitting_multicast", pyvicon_start_transmitting_multicast, METH_VARARGS, "Start multicast server transmission"},
+         {"pyvicon_stop_transmitting_multicast", pyvicon_stop_transmitting_multicast, METH_VARARGS, "Stop multicast server transmission"},
+         {"pyvicon_set_buffer_size", pyvicon_set_buffer_size, METH_VARARGS, "Set Frame buffer size"},
          {NULL, NULL, 0, NULL}
     };
 
@@ -68,6 +196,10 @@ extern "C" {
     };
     #define INITERROR return NULL
 
+
+    //########################
+    // Module init function
+    //########################
     PyMODINIT_FUNC PyInit_pyvicon_module(void) {
         PyObject* m;
         PyObject *module = PyModule_Create(&moduledef);
--- pyvicon/pyvicon.py
+++ pyvicon/pyvicon.py
@@ -1,10 +1,66 @@
 import pyvicon_module
 
+from enum import Enum
+
+
+class Result(Enum):
+    Unknown = 0
+    NotImplemented = 1
+    Success = 2
+    InvalidHostName = 3
+    InvalidMulticastIP = 4
+    ClientAlreadyConnected = 5
+    ClientConnectionFailed = 6
+    ServerAlreadyTransmittingMulticast = 7
+    ServerNotTransmittingMulticast = 8
+    NotConnected = 9
+    NoFrame = 10
+    InvalidIndex = 11
+    InvalidCameraName = 12
+    InvalidSubjectName = 13
+    InvalidSegmentName = 14
+    InvalidMarkerName = 15
+    InvalidDeviceName = 16
+    InvalidDeviceOutputName = 17
+    InvalidLatencySampleName = 18
+    CoLinearAxes = 19
+    LeftHandedAxes = 20
+    HapticAlreadySet = 21
+
 
 class PyVicon:
     def __init__(self):
         self.client_ = pyvicon_module.new_client()
+        major, minor, point = pyvicon_module.pyvicon_version(self.client_)
+        self.__version__ = "{}.{}.{}".format(major, minor, point)
 
+    def connect(self, ip):
+        return Result(pyvicon_module.pyvicon_connect(self.client_, ip))
+
+    def connect_multicast(self, ip, multicast):
+        return Result(pyvicon_module.pyvicon_connect_to_multicast(self.client_, ip, multicast))
+
+    def disconnect(self):
+        return Result(pyvicon_module.pyvicon_disconnect(self.client_))
+
+    def is_connected(self):
+        return pyvicon_module.pyvicon_isconnected(self.client_)
+
+    def start_server_multicast(self, server_ip, multicast_ip):
+        return Result(pyvicon_module.pyvicon_start_transmitting_multicast(self.client_, server_ip, multicast_ip))
+
+    def stop_server_multicast(self):
+        return Result(pyvicon_module.pyvicon_stop_transmitting_multicast(self.client_))
+
+    def set_buffer_size(self, size):
+        pyvicon_module.pyvicon_set_buffer_size(self.client_, size)
 
 if __name__ == '__main__':
     test = PyVicon()
+    print("SDK version : {}".format(test.__version__))
+    print("Connection status : {}".format(test.is_connected()))
+    print(test.connect("127.0.0.1"))
+    print(test.connect_multicast("127.0.0.1", "224.0.0.0"))
+    print(test.disconnect())
+    print(test.start_server_multicast("localhost", "224.0.0.0"))
+    print(test.stop_server_multicast())
